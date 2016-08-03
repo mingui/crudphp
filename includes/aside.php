3e9d768da80f83a51dbdf063446dd6f5f34e332c
Jesus Peralta <gnumux@gmail.com>
2016-08-02 20:37:05 -0400

    Se agrego los enlaces en el aside.php

--- includes/aside.php
+++ includes/aside.php
@@ -3,4 +3,5 @@
   <li><a href="index.php">Principal</a></li>
   <li><a href="usuarios.php">Usuarios</a></li>
     <li><a href="cms.php">Cms</a></li>
+    <li><a href="slider.php">Slider</a></li>
 </ul>
